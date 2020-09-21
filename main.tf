b7f67ac6f5b0c26cfd342f96894a937ac2ada1a5
Krishna Valaboju <kvalaboju@sxiq.com.au>
2020-09-21 09:31:42 +0000

    added new accounts

--- main.tf
+++ main.tf
@@ -81,3 +81,184 @@ module "logicmonitor_apac_poc" {
   }
 }
 
+
+module "logicmonitor_network" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "network"
+  providers = {
+    aws = aws.network
+  }
+}
+
+
+module "logicmonitor_nala-prd" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "nala-prd"
+  providers = {
+    aws = aws.nala-prd
+  }
+}
+
+
+module "logicmonitor_pubweb-dev" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "pubweb-dev"
+  providers = {
+    aws = aws.pubweb-dev
+  }
+}
+
+module "logicmonitor_contactcenter-dev" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "contactcenter-dev"
+  providers = {
+    aws = aws.contactcenter-dev
+  }
+}
+
+module "logicmonitor_security" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "security"
+  providers = {
+    aws = aws.security
+  }
+}
+
+
+module "logicmonitor_nala-network" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "nala-network"
+  providers = {
+    aws = aws.nala-network
+  }
+}
+
+
+module "logicmonitor_contactcenter-tst" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "contactcenter-tst"
+  providers = {
+    aws = aws.contactcenter-tst
+  }
+}
+
+module "logicmonitor_nala-shared" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "nala-shared"
+  providers = {
+    aws = aws.nala-shared
+  }
+}
+
+
+module "logicmonitor_pubweb-tst" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "pubweb-tst"
+  providers = {
+    aws = aws.pubweb-tst
+  }
+}
+
+module "logicmonitor_servicedesk" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "servicedesk"
+  providers = {
+    aws = aws.servicedesk
+  }
+}
+
+
+module "logicmonitor_shared-ent" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "shared-ent"
+  providers = {
+    aws = aws.shared-ent
+  }
+}
+
+module "logicmonitor_emea-shared" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "emea-shared"
+  providers = {
+    aws = aws.semea-shared
+  }
+}
+
+module "logicmonitor_pubweb-prd" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "pubweb-prd"
+  providers = {
+    aws = aws.pubweb-prd
+  }
+}
+
+module "logicmonitor_apac-network" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "apac-network"
+  providers = {
+    aws = aws.apac-network
+  }
+}
+
+
+module "logicmonitor_emea-prd" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "emea-prd"
+  providers = {
+    aws = aws.emea-prd
+  }
+}
+
+
+module "logicmonitor_emea-dev" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "emea-dev"
+  providers = {
+    aws = aws.emea-dev
+  }
+}
+
+
+module "logicmonitor_emea-network" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "emea-network"
+  providers = {
+    aws = aws.emea-network
+  }
+}
+
+
+module "logicmonitor_apac-prd" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "apac-prd"
+  providers = {
+    aws = aws.apac-prd
+  }
+}
+
+module "logicmonitor_master" {
+  source                         = "git::git@gitlab.com:orica/terraform-modules/logicmonitor.git?ref=v0.1.0"
+  enable_logicmonitor = local.enable_logicmonitor
+  account_alias                  = "master"
+  providers = {
+    aws = aws.master
+  }
+}
