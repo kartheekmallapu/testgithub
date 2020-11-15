37a3bf5beab5a652e88814ba10e1fbc129cf29a3
Kartheek Mallapu <kmallapu@sxiq.com.au>
2020-11-15 22:56:37 +0000

    Update lm-addaccount.py

--- AddAccount/lm-addaccount.py
+++ AddAccount/lm-addaccount.py
@@ -30,7 +30,9 @@ epoch = str(int(time.time() * 1000))
 requestVars = httpVerb + epoch + data + resourcePath
 
 #Construct signature
-signature = base64.b64encode(hmac.new(b'AccessKey',msg=requestVars,digestmod=hashlib.sha256).hexdigest())
+dig = hmac.new(b'AccessKey',msg=requestVars,digestmod=hashlib.sha256).hexdigest()
+#signature = base64.b64encode(hmac.new(b'AccessKey',msg=requestVars,digestmod=hashlib.sha256).hexdigest())
+signature = base64.b64encode(dig).decode()
 
 #Construct headers
 auth = 'LMv1 ' + AccessId + ':' + signature + ':' + epoch
